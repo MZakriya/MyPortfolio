e8c4fc4be27201dade4b869d44e6f45954258628
MZakriya <ziky989@gmail.com>
2025-03-12 15:07:57 +0500

    Fix: Allow external image domains

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
-import type { NextConfig } from "next";
+import { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  images: {
+    domains: ["picsum.photos", "randomuser.me"], 
+  },
 };
 
 export default nextConfig;
